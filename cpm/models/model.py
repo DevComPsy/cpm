b8598d5d841f753094b929ff450b24ff2954c8a3
lenarddome <lenarddome@gmail.com>
2024-01-12 11:46:07 +0100

    create model compiler

--- cpm/models/model.py
+++ cpm/models/model.py
@@ -1,28 +1,72 @@
 # This file contains the model and generator classes for the model
 import numpy as np
-from .zoo import utils
 
 class Model:
-    def __init__(self, functions = None):
+    """
+    A class representing a model.
+
+    Attributes:
+        functions (list): A list of functions used in the model.
+        values (dict): A dictionary containing the computed values of the model.
+        policy (str): The policy computed by the model.
+
+    Methods:
+        compute: Computes the model based on the provided arguments for one single trial.
+    """
+
+    def __init__(self, functions=None):
         self.functions = functions
+        self.values = []
+        self.policy = None
 
     def compute(self, **kwargs):
-        # TODO: save outputs
+        """
+        Computes the model based on the provided arguments.
+
+        Args:
+            **kwargs: Keyword arguments used by the functions in the model. It must
+            contain all the Named Arguments required by the functions supplied in the model.
+
+        Returns:
+            str: The computed policy.
+
+        """
+        outputs = {}
+        x = None
+        policy = None
         for function in self.functions:
-            locator = function.config()['locator']
-            if function.config()['type'] == 'decision':
-                policy = function(**kwargs)
+            init = function(**kwargs)
+            name = init.config()['name']
+            type = init.config()['type']
+            if type == 'decision':
+                init.compute()
+                outputs[name] = init.policies
+                policy = init.policies
+                print(policy)
+                print(init.policies)
             else:
-                outputs.append(index) = function(**kwargs)
-        return x, policy
+                init.compute()
+                x = init.weights
+                outputs[name] = x
+        self.values = outputs
+        return policy
 
-    def get_config(self):
-        return None
 
-    def extract_layers(self, names):
-        
-        
+# arguments = {
+#     'alpha' : 0.1,
+#     'temperature': 1,
+#     'weights' : np.array([[0.5, 0], [0, 0.5]]),
+#     'input' : np.array([1, 1]),
+#     'teacher' : np.array([1, 0]),
+#     'attention' : np.array([1, 0]),
+#     'misc' : np.array([1, 0])
+#     }
 
-class Generator:
-    def __init__(self, *args, **kwargs):
-        super(CLASS_NAME, self).__init__(*args, **kwargs)
+# input = arguments
+
+
+# mine = Model(functions = [LinearActivation, Softmax, DeltaRule])
+# mine.compute(**input)
+
+    # def get_config(self):
+    #     return None

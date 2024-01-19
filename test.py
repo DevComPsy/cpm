4d74a1e7a968f55dda09d2ecf554fe933a04c7e0
lenarddome <lenarddome@gmail.com>
2024-01-19 11:14:04 +0100

    uploade temporary test file

--- test.py
+++ test.py
@@ -1,68 +1,126 @@
-from cpm.models import RescorlaWagner, Simulator  # import model
-from cpm.optimisation import DifferentialEvolution  # import optimisation
-from cpm.optimisation import utils  # import utils
-from prettyformatter import pprint
+from cpm.models import Model, Wrapper, Simulator
+from cpm.models.zoo.activation import LinearActivation
+from cpm.models.zoo.decision import Softmax
+from cpm.models.zoo.learning import DeltaRule
+from cpm.optimisation import DifferentialEvolution, minimise
+from cpm.evaluation import strategies, ParameterRecovery
+from prettyformatter import pprint as pp
+import pickle as pkl
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
 data = {
-    "trials": np.array([[2, 3], [1, 4], [2, 3], [1, 4], [2, 3]]),
-    "feedback": np.array([1, 2, 1, 2, 1]),
-    "observed": np.array([[1, 0], [0, 1], [1, 0], [0, 1], [1, 0]]),
+        "trials": np.array([[2, 3], [1, 4], [3, 2], [4, 1], [2, 3], [2, 3], [1, 4], [3, 2], [4, 1], [2, 3]]),
+        "feedback": np.array([1, 2, 1, 2, 1, 1, 2, 1, 2, 1]),
+        "observed": np.array([[1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [1, 0], [0, 1], [1, 0], [0, 1], [1, 0]]),
 }
 
-pp.pprint(data)
+pp(data)
 
 params = {  # define the parameters
     "alpha": 0.25,
     "temperature": 5,
-    'weights': [[0.5, 0.5, 0.5, 0.5], [0.5, 0.5, 0.5, 0.5]],
 }
 
-RW = RescorlaWagner(data, params) # initialize the model
-RW.run()  # run the model
-RW.policies  # investigate TODO: rename to policies
+arguments = {
+    'alpha' : 0.1,
+    'temperature': 1,
+    'weights' : np.array([[0.5, 0.1], [0, 0.5]]),
+    'input' : np.array([0, 1]),
+    'feedback' : np.array([0, 1]),
+    'attention' : np.array([1, 0]),
+    'misc' : np.array([1, 0])
+    }
 
-pprint(RW.export())  # export the results
+input = arguments.copy()
 
+# The code is creating an instance of the `Model` class and initializing it with a
+# list of functions `[LinearActivation, Softmax, DeltaRule]`. Then, it calls the
+# `compute` method of the `Model` instance, passing the `input` dictionary as
+# keyword arguments. This method computes the model based on the provided
+# arguments. Finally, it calls the `summary` method of the `Model` instance, which
+# prints a summary of the model.
 
-# run the simulator on multiple ppt
+mine = Model(functions = [LinearActivation, Softmax, DeltaRule])
+mine.compute(**input)
+pp(mine.policy)
+pp(mine.summary())
+mine.values
 
+pkl.dump(mine, open("mine.pkl", "wb"))
+pickled_model = pkl.load(open("mine.pkl", "rb"))
+
+pickled_model.policy
+pp(pickled_model.values)
+pickled_model.summary()
+
+del pickled_model
+
+x = Wrapper(model = mine, data = data, params = params)
+x.run()
+pp(x.summary())
+pp(x.export())
+
+x.reset()
+pp(x.summary())
 ## set up multiple people
 experiment = []
 for i in range(100):
     ppt = {
         "ppt": i,
-        "trials": np.array([[2, 3], [1, 4], [3, 2], [4, 1], [2, 3]]),
-        "feedback": np.array([1, 2, 1, 2, 1]),
-        "observed": np.array([[1, 0], [0, 1], [1, 0], [0, 1], [1, 0]]),
+        "trials": np.array([[2, 3], [1, 4], [3, 2], [4, 1], [2, 3], [2, 3], [1, 4], [3, 2], [4, 1], [2, 3]]),
+        "feedback": np.array([1, 2, 1, 2, 1, 1, 2, 1, 2, 1]),
+        "observed": np.array([[1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [1, 0], [0, 1], [1, 0], [0, 1], [1, 0]]),
     }
     experiment.append(ppt)
 
-## set up multiple parameters
-parameters = []
-for i in range(len(experiment)):
-    params = {
-        "alpha": np.random.uniform(0, 1),
-        "temperature": np.random.uniform(1, 5),
-    }
-    parameters.append(params)
+lower = [1e-10, 1e-10]
+upper = [1, 10]
+bounds = list(zip(lower, upper))
 
-Simulation = Simulator(RescorlaWagner, experiment, parameters)
-Simulation.run()
-pprint(Simulation.simulation)
-pprint(Simulation.probabilities())
-
-pro = Simulation.probabilities()
-utils.ConvertToPandas(pro)
-
-Optimise = DifferentialEvolution(
-    RescorlaWagner, experiment, minimisation="LogLikelihood"
+# FIXME: this is not working, metric is always overwritten
+Fit = DifferentialEvolution(
+    model = x, bounds = bounds, data = experiment, minimisation="LogLikelihood", mutation = 0.5, recombination = 0.7, strategy = "best1bin", tol = 0.001
 )  # initialize the optimisation
-Optimise.minimise([0.5, 2])
+Fit.minimise([0.60924690133760082, 5.6764639071806062])
+Fit.optimise()
 
-Optimise.optimise()
+pp(Fit.fit)
 
-pprint(Optimise.fit)
+fittings = [item.get('fun') for item in Fit.fit]
+pp(fittings)
+pp(Fit.parameters)
 
-pd.DataFrame(Optimise.fit)
+explore = Simulator(model = x, data = experiment, parameters = Fit.parameters)
+explore.run()
+explore.policies()
+explore.generate()
+pp(explore.generated)
+
+explore.update(parameters = Fit.parameters[0])
+
+explore.run()
+explore.policies()
+pp(explore.parameters)
+
+recover = ParameterRecovery(
+    model = explore, optimiser = "DifferentialEvolution", strategy = "grid",
+    bounds = bounds, iteration = 2
+    )
+recover.recover()
+# dimensions are: iteration, recovered/original, population (trial order)
+alpha = recover.extract(key = 'alpha')
+
+recovered = np.asarray(alpha[:, 0, :]).flatten()
+original = np.asarray(alpha[:, 1, :]).flatten()
+
+
+plt.scatter(recovered, original)
+plt.show()
+
+temperature = recover.extract(key = 'temperature')
+recovered_temp = np.asarray(temperature[:, 0, :]).flatten()
+original_temp = np.asarray(temperature[:, 1, :]).flatten()
+
+plt.scatter(recovered_temp, original_temp)
+plt.show()

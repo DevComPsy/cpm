849d3c321aa7df7cb3fc6b61597fca0778c416eb
lenarddome <lenarddome@gmail.com>
2024-02-05 08:50:12 +0100

    🚚 move competitive attentional gating to attention section

--- cpm/components/attention.py
+++ cpm/components/attention.py
@@ -1,34 +1,63 @@
-"""
-Attentional processes for learning
-"""
-
-import numpy as np
-
 class CompetitiveGating:
+    """
+    An activation function that incorporates stimulus salience in addition to the stimulus vector to modulate the weights.
+    It formalises the hypothesis that each stimulus has an underlying salience that competes to captures attentional focus (Paskewitz and Jones, 2020; Kruschke, 2001).
 
-    def __init__(self, brutality = None, input = None, weights = None, attention = None, **kwargs):
+    Parameters
+    ----------
+    input : array_like
+        The input value. The stimulus representation (vector).
+    values : array_like
+        The values. A 2D array of values, where each row represents an outcome and each column represents a single stimulus.
+    salience : array_like
+        The salience value. A 1D array of salience values, where each value represents the salience of a single stimulus.
+    P : float
+        The power value, also called attentional normalisation or brutality, which influences the degree of attentional competition.
+
+    Attributes
+    ----------
+    input : array_like
+        The input value. The stimulus representation (vector).
+    values : array_like
+        The values value. A 2D array of values, where each row represents an outcome and each column represents a single stimulus.
+    salience : array_like
+        The salience value. A 1D array of salience values, where each value represents the salience of a single stimulus.
+    P : float
+        The power value, also called attentional normalisation or brutality. It influences the degree of attentional competition.
+    gain : array_like
+        The normalised attentional gain for each stimulus, corresponding to the input vector.
+
+    Examples
+    --------
+    >>> input = np.array([1, 1, 0])
+    >>> values = np.array([[0.1, 0.9, 0.8], [0.6, 0.2, 0.1]])
+    >>> salience = np.array([0.1, 0.2, 0.3])
+    >>> att = AttentionalActivation(input, values, salience, P = 1)
+    >>> att.compute()
+    array([[0.03333333, 0.6       , 0.        ],
+           [0.2       , 0.13333333, 0.        ]])
+    """
+
+    def __init__(self, input=None, values=None, salience=None, P=1, **kwargs):
         self.input = input
-        self.attention = attention.copy()
-        self.weights = weights.copy()
-        self.brutality = brutality
-        self.gain = None
+        self.values = values.copy()
+        self.salience = salience.copy()
+        self.P = P
+        self.gain = []
 
     def compute(self):
-        """ Competitive gating function
         """
-        self.gain = self.input * self.attention
-        self.gain = self.gain / np.sum(self.gain ** self.brutality) ** (1/self.brutality)
-        for i in range(self.weights.shape[0]):
-            self.weights[i] = self.weights[i] * self.gain[i]
-        return self.gain
+        Compute the attentional activation function.
 
-# input = {
-#     'brutality' : 2,
-#     'weights' : np.array([[0.5, 0.1], [0.9, 0.5]]),
-#     'input' : np.array([1, 1]),
-#     'attention' : np.array([1, 2]),
-#     }
-
-# x = CompetitiveGating(**input)
-# x.compute()
-# x.weights
+        Returns
+        -------
+        array_like
+            The values updated with the attentional gain and stimulus vector.
+        """
+        self.gain = self.input * self.salience
+        self.gain = self.gain**self.P
+        self.gain = self.gain / np.sum(self.gain) ** (1 / self.P)
+        for i in range(self.values.shape[0]):
+            for k in range(self.values.shape[1]):
+                self.values[i, k] = self.values[i, k] * self.gain[k]
+        return self.values

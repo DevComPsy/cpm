bcd1495076ba1860dea0da4dc0aed2811d29c832
lenarddome <lenarddome@gmail.com>
2024-03-11 11:50:42 +0100

    exp: start building fmin to accommodate parallelisation

--- cpm/optimisation/fmin.py
+++ cpm/optimisation/fmin.py
@@ -1,10 +1,50 @@
 from scipy.optimize import fmin
-import numpy as np
-import pandas as pd
-import copy
 from . import minimise
 from . import utils
 from ..generators import Simulator, Wrapper
+
+import numpy as np
+import pandas as pd
+import copy
+import dask
+
+
+def minimum(pars, function, data, loss, **args):
+    """
+    The `minimise` function calculates a metric by comparing predicted values with
+    observed values.
+
+    Parameters
+    ----------
+    pars
+        The `pars` parameter is a dictionary that contains the parameters for the
+        function that needs to be minimized.
+    function
+        The `function` parameter is the function that needs to be minimized.
+    data
+        The `data` parameter is the data that is used to compare the predicted values
+        with the observed values.
+    loss
+        The `loss` parameter is the loss function that is used to calculate the metric
+        value.
+    args
+        The `args` parameter is a dictionary that contains additional parameters that
+        are used in the loss function.
+
+    Returns
+    -------
+        The metric value is being returned.
+
+    """
+    function.reset(pars)
+    function.run()
+    predicted = function.dependent
+    observed = data
+    metric = loss(predicted, observed, **args)
+    del predicted, observed
+    if metric == float("inf") or metric == float("-inf") or metric == float("nan"):
+        metric = 1e10
+    return metric
 
 
 class Fmin:
@@ -57,7 +97,7 @@ class Fmin:
         minimisation=minimise.LogLikelihood.continuous,
         **kwargs
     ):
-        self.function = copy.deepcopy(model)
+        self.model = copy.deepcopy(model)
         self.data = data
         self.loss = minimisation
         self.initial_guess = initial_guess
@@ -67,7 +107,7 @@ class Fmin:
         self.parameters = []
         self.participant = data[0]
         if isinstance(model, Wrapper):
-            self.parameter_names = self.function.parameter_names
+            self.parameter_names = self.model.parameter_names
         if isinstance(model, Simulator):
             raise ValueError(
                 "The Fmin algorithm is not compatible with the Simulator object."
@@ -77,32 +117,36 @@ class Fmin:
             "k": len(self.initial_guess),
         }
 
-    def minimise(self, pars, **args):
-        """
-        The `minimise` function calculates a metric by comparing predicted values with
-        observed values.
+    # def minimise(self, pars, **args):
+    #     """
+    #     The `minimise` function calculates a metric by comparing predicted values with
+    #     observed values.
 
-        Parameters
-        ----------
-        pars
-            The `pars` parameter is a dictionary that contains the parameters for the
-            function that needs to be minimized.
+    #     Parameters
+    #     ----------
+    #     pars
+    #         The `pars` parameter is a dictionary that contains the parameters for the
+    #         function that needs to be minimized.
 
-        Returns
-        -------
-            The metric value is being returned.
+    #     Returns
+    #     -------
+    #         The metric value is being returned.
 
-        """
-        evaluated = copy.deepcopy(self.function)
-        evaluated.reset(pars)
-        evaluated.run()
-        predicted = evaluated.dependent
-        observed = self.participant.get("observed")
-        metric = self.loss(predicted, observed, **self.auxiliary)
-        del predicted, observed
-        if metric == float("inf") or metric == float("-inf") or metric == float("nan"):
-            metric = 1e10
-        return metric
+    #     """
+    #     evaluated = copy.deepcopy(self.function)
+    #     evaluated.reset(pars)
+    #     evaluated.run()
+    #     predicted = evaluated.dependent
+    #     observed = self.participant.get("observed")
+    #     metric = self.loss(predicted, observed, **self.auxiliary)
+    #     del predicted, observed
+    #     if metric == float("inf") or metric == float("-inf") or metric == float("nan"):
+    #         metric = 1e10
+    #     return metric
+
+    def __single(self, do):
+
+        return None
 
     def optimise(self):
         """
@@ -119,25 +163,53 @@ class Fmin:
                 out[keys[i]] = x[i]
             return out
 
-        for i in range(len(self.data)):
-            self.participant = self.data[i]
-            self.function.data = self.participant
-            # objective = copy.deepcopy(self.minimise)
+        def __task(model, participant, loss, **args):
             result = fmin(
-                self.minimise,
+                minimum,
                 x0=self.initial_guess,
+                args=(model, participant.get("observed"), loss),
                 disp=False,
                 **self.kwargs,
                 full_output=True
             )
-            # add the parameters to the list
-            self.details.append(__unpack(copy.deepcopy(result)))
-            parameters = {}
-            for i in range(len(self.initial_guess)):
-                parameters[self.parameter_names[i]] = result[0][i]
-            self.parameters.append(parameters)
-            # add the results to the list
-            self.fit.append({"parameters": result[0], "fun": copy.deepcopy(result[1])})
+            return result
+
+        from dask.distributed import Client, progress
+
+        client = Client(threads_per_worker=4, n_workers=1)
+
+        # # Submit work to happen in parallel
+        # results = []
+        # for filename in filenames:
+        #     data = client.submit(load, filename)
+        #     result = client.submit(process, data)
+        #     results.append(result)
+
+        # # Gather results back to local computer
+        # results = client.gather(results)
+        distribution = []
+        for i in range(len(self.data)):
+            self.participant = self.data[i]
+            self.model.data = self.participant
+            tt = client.submit(__task)(
+                self.model,
+                self.participant,
+                self.loss,
+            )
+            distribution.append(tt)
+        self.details = distribution
+        # self.details = client.gather(distribution)
+        # self.details = dask.compute(*distribution)
+
+        # add the parameters to the list
+        # result = __task(self.model, self.participant, self.loss, **self.auxiliary)
+        # self.details.append(__unpack(copy.deepcopy(result)))
+        # parameters = {}
+        # for i in range(len(self.initial_guess)):
+        #     parameters[self.parameter_names[i]] = result[0][i]
+        # self.parameters.append(parameters)
+        # # add the results to the list
+        # self.fit.append({"parameters": result[0], "fun": copy.deepcopy(result[1])})
         return None
 
     def reset(self):
@@ -145,7 +217,7 @@ class Fmin:
         Resets the optimization results and fitted parameters.
 
         Returns:
-        - None
+            None
         """
         self.fit = []
         self.parameters = []

a62c16891f5d7f35268fab3aa89bc0c2e30f9b8b
lenarddome <lenarddome@gmail.com>
2024-05-08 15:21:09 +0200

    complete empirical bayes up until log model evidence

--- cpm/optimisation/hierarchical.py
+++ cpm/optimisation/hierarchical.py
@@ -14,15 +14,12 @@ class EmpiricalBayes:
     ----------
     optimiser : object
         The initialized Optimiser object. It must use an optimisation algorithm that also returns the Hessian matrix.
-    population_parameters : Parameters
-        A list of the group-level parameters that will be optimised using the subject-level parameters. It must be a Parameters object.
 
     """
 
     def __init__(
         self,
         optimiser=None,
-        population_parameters=None,
         iteration=1000,
         tolerance=1e-6,
         chain=4,
@@ -31,7 +28,6 @@ class EmpiricalBayes:
     ):
         self.function = copy.deepcopy(optimiser.function)
         self.optimiser = copy.deepcopy(optimiser)
-        self.pop_parameters = population_parameters
         # bounds here should include mean and std for all parameters
         self.iteration = iteration  # maximum number of iterations
         self.tolerance = tolerance  # tolerance for convergence
@@ -46,10 +42,15 @@ class EmpiricalBayes:
 
     def step(self):
         self.optimiser.run()
-        hessian = []
-        for n in self.optimiser.details:
-            hessian.append(n.get("hessian"))
-        hessian = np.array(hessian)
+        hessian = np.zeros(
+            (
+                len(self.optimiser.details),
+                len(self.optimiser.parameter_names),
+                len(self.optimiser.parameter_names),
+            )
+        )
+        for i, n in enumerate(self.optimiser.fit):
+            hessian[i] = n.get("hessian")
         return self.optimiser.parameters, hessian, self.optimiser.details
 
     def chain(self):
@@ -57,26 +58,42 @@ class EmpiricalBayes:
         log_model_evidence = []
         lme_old = 0
 
+        # TODO: prior setups, calculate group-level means and stds
+
+        population_prior = self.optimiser.model.parameters.PDF(log=True)
+
         for iteration in range(self.iteration):
-            # TODO: prior setups, calculate group-level means and stds
+            parameters, hessian, details = self.step()
+            self.fit.append(copy.deepcopy(parameters))
+            self.details.append(copy.deepcopy(details))
+            self.optimiser.reset()
 
-            population_prior = self.optimiser.model.parameters.PDF(log=True)
+            # DONE: transform negative log likelihood into maximum log likelihood
+            mll = np.asarray([-ppt.get("fopt") for ppt in self.fit])
+            # TODO: HIHG-LEVEL update group-level means and stds
 
-            for iteration in range(self.iteration):
-                parameters, hessian, details = self.step()
-                self.fit.append(copy.deepcopy(parameters))
-                self.details.append(copy.deepcopy(details))
-                self.optimiser.reset()
-
-            # TODO: transform negative log likelihood into maximum log likelihood
-            # TODO: update group-level means and stds
             # TODO: group-level mean is the avarage across participants
+            parameter_names = self.optimiser.model.parameters.free()
+            param = np.zeros((len(parameters), len(parameter_names)))
+            for i, name in enumerate(parameter_names):
+                for ppt, content in enumerate(parameters):
+                    param[ppt, i] = content.get(name)
+            means = param.mean(axis=0)
             # TODO: std: first calculate variance (between-subjects squared differences)
+            variance = param.var(axis=0)
             # TODO: std: second calculate inverse hessian matrix
+            inv_hessian = np.asarray(list(map(np.linalg.inv, hessian)))
+
             # TODO: std: third take the diagonal element of the inverse hessian matrix
+            diagonal = np.diagonal(inv_hessian, axis1=1, axis2=2)
             # TODO: std: fourth add up variance plus diagonal for each ppt and then add up each ppt and divide them by mean
+            between_within_variance = (
+                np.square(param - means) + diagonal
+            ) / param.shape[0]
             # TODO: std: fifth subtract the squared mean from result of the above four steps
+            between_within_variance -= np.square(means)
             # TODO: std: make sure the std is not too small by bounding it to 1e-6
+            np.clip(between_within_variance, 1e-6, None, out=between_within_variance)
 
             # TODO: estimate log model evidence (lme)
             lme = []
@@ -84,6 +101,14 @@ class EmpiricalBayes:
             # TODO: lme: second sum up the penalised log determinants and add
             # TODO: lme: third penalise them by the number of parameters (param * log(2 *pi))
             # TODO: lme: sum up ppt lme and add BIC penalty term - n_params * log(data_points)
+
+            population_updates = {}
+
+            for key, value in parameters:
+                # NOTE: code is placeholder - dimensions in value will be different
+                population_updates.update({key: {"mean": value[0], "std": value[1]}})
+
+            self.optimiser.model.parameters.update_prior(**population_updates)
 
             if iteration > 0:
                 if np.abs(lme - lme_old) < self.tolerance:

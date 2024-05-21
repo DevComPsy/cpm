258a33d3340ab353c5c37654db11a5554191c0b3
lenarddome <lenarddome@gmail.com>
2024-05-21 15:53:46 +0200

    style: clean up and cross-reference empirical bayes with Gershman paper

--- cpm/optimisation/empirical.py
+++ cpm/optimisation/empirical.py
@@ -11,11 +11,24 @@ class EmpiricalBayes:
     ----------
     optimiser : object
         The initialized Optimiser object. It must use an optimisation algorithm that also returns the Hessian matrix.
-
+    objective : str
+        The objective of the optimisation, either 'maximise' or 'minimise'. Default is 'minimise'. Only affects how we arrive at the participant-level _a posteriori_ parameter estimates.
+    initial_guesses_matrix : numpy.ndarray, optional
+        A matrix of initial guesses for the parameters. The matrix should have the shape (chain, number of parameters). Default is None.
+    iteration : int, optional
+        The maximum number of iterations. Default is 1000.
+    tolerance : float, optional
+        The tolerance for convergence. Default is 1e-6.
+    chain : int, optional
+        The number of random parameter initialisations. Default is 4.
 
     Notes
     -----
     The EmpiricalBayes class implements an Expectation-Maximisation algorithm for the optimisation of the group-level distributions of the parameters of a model from subject-level parameter estimations. The algorithm is based on the following steps:
+
+    The fitting function must return the Hessian matrix of the optimisation. The Hessian matrix is a square matrix of second-order partial derivatives of the negative log-likelihood function. The Hessian matrix is used to calculate the within-subject variance of the parameters. Compatible methods in the toolbox include Fmin and Fminbound.
+
+    It is also important to note that we will require the Hessian matrix of second derivatives of the negative log posterior (Gershman, 2016, p. 3). This requires us to minimise or maximise the log posterior density as opposed to a simple log likelihood, when estimating participant-level parameters.
 
     INSERT IMAGE FROM PAPER
 
@@ -31,6 +44,7 @@ class EmpiricalBayes:
         iteration=1000,
         tolerance=1e-6,
         chain=4,
+        objective="minimise",
         **kwargs,
     ):
         self.function = copy.deepcopy(optimiser.model)
@@ -39,9 +53,12 @@ class EmpiricalBayes:
         self.iteration = iteration  # maximum number of iterations
         self.tolerance = tolerance  # tolerance for convergence
         self.chain = chain  # number of random parameter initialisations
-        self.kwargs = kwargs
-
+        self.objective = (
+            objective  # whether the optimiser looks for the minimum or maximum
+        )
         self.__number_of_parameters__ = len(self.optimiser.model.parameters.free())
+
+        self.kwargs = kwargs
 
         self.fit = []
         self.details = []
@@ -57,6 +74,7 @@ class EmpiricalBayes:
         return self.optimiser.parameters, hessian, self.optimiser.fit
 
     def stair(self):
+        ## Equation Numbers refer to Equations in the Gershman (2016) Empirical priors for reinforcement learning models
         ## Step numbers correspond to
         lme_old = 0
         lmes = []
@@ -65,16 +83,19 @@ class EmpiricalBayes:
             prior = self.optimiser.model.parameters.PDF()
 
             self.optimiser.reset()
+            self.optimiser.initial_guess = self.initial_guesses_matrix[iteration]
 
             parameters, hessian, details = self.step()
             self.fit.append(copy.deepcopy(parameters))
             self.details.append(copy.deepcopy(details))
 
-            # DONE: transform negative log likelihood into maximum log likelihood
-            negative_log_posterior = np.asarray([-ppt.get("fopt") for ppt in details])
-            negative_log_posterior = negative_log_posterior + prior
+            # extract and transform negative log posteriors into maximum log likelihood
+            # this essentially gives us the output of Equation 5
+            negative_log_posterior = np.asarray([ppt.get("fopt") for ppt in details])
+            if self.objective == "minimise":
+                negative_log_posterior = -1 * negative_log_posterior
 
-            # TODO: group-level mean is the avarage across participants
+            # get group-level mean across participants
             parameter_names = self.optimiser.model.parameters.free()
             param = np.zeros((len(parameters), len(parameter_names)))
             for i, name in enumerate(parameter_names):
@@ -82,35 +103,25 @@ class EmpiricalBayes:
                     param[ppt, i] = content.get(name)
 
             means = param.mean(axis=0)
-            # TODO: std: first calculate variance (between-subjects squared differences)
+            ## In what follows, we break down Equation 6
+            # first calculate variance (between-subjects squared differences)
             variance = param.var(axis=0)
-            # TODO: std: second calculate inverse hessian matrix
+
+            ## getting the diagonal of the inverse hessian matrix
+            ## 1. calculate inverse hessian matrix
             inv_hessian = np.asarray(list(map(np.linalg.inv, hessian)))
-
-            # TODO: std: third take the diagonal element of the inverse hessian matrix
+            ## 2. take the diagonal element of the inverse hessian matrix
             diagonal = np.diagonal(inv_hessian, axis1=1, axis2=2)
-            # TODO: std: fourth add up variance plus diagonal for each ppt and then add up each ppt and divide them by mean
-            between_within_variance = (
-                np.square(param - means) + diagonal
-            ) / param.shape[0]
-            # TODO: std: make sure the std is not too small by bounding it to 1e-6
-            np.clip(between_within_variance, 1e-6, None, out=between_within_variance)
-            between_within_variance = between_within_variance.mean(axis=0)
-            # TODO: std: fifth subtract the squared mean from result of the above four steps
-            variance = between_within_variance.mean(axis=0) - np.square(means)
-            # TODO: estimate log model evidence (lme)
-            # TODO: lme: first find the log determinant of the hessian matrix for each ppt
-            log_determinants = (np.asarray(list(map(np.linalg.slogdet, hessian)))).sum(
-                axis=0
-            )[1]
-            penalty = 0.5 * (
-                self.__number_of_parameters__ * np.log(2 * np.pi) - log_determinants
-            )
-            log_model_evidence = negative_log_posterior + penalty
-            # TODO: lme: penalty term - n_params * log(data_points)
-            # TODO: lme: third penalise them by the number of parameters (param * log(2 *pi)) - I will skip this step
-            # TODO: lme: second sum up the penalised log determinants
 
+            ## calculate the variance of the between-subjects squared differences and the within-subjects squared differences
+            ## add up variance plus diagonal for each ppt and then add up each ppt and divide them by mean
+            between_within_variance = np.square(param - means) + diagonal
+            ## make sure the STD is not too small by bounding it to 1e-6
+            np.clip(between_within_variance, 1e-6, None, out=between_within_variance)
+            ## get the mean of the between_within_variance minus the
+            variance = between_within_variance.mean(axis=0) - np.square(means)
+
+            ## update population-level parameters.
             population_updates = {}
 
             for i, name in enumerate(parameter_names):
@@ -121,8 +132,19 @@ class EmpiricalBayes:
 
             self.optimiser.model.parameters.update_prior(**population_updates)
 
+            # estimate log model evidence (lme)
+            # first find the log determinant of the hessian matrix for each ppt
+            log_determinants = (np.asarray(list(map(np.linalg.slogdet, hessian)))).sum(
+                axis=0
+            )[1]
+            ## add a penalty term to the negative log posterior
+            penalty = 0.5 * (
+                self.__number_of_parameters__ * np.log(2 * np.pi) - log_determinants
+            )
+            ## calculate the log model evidence with a penalty and sum them up
+            log_model_evidence = negative_log_posterior + penalty
             summed_lme = log_model_evidence.sum()
-
+            ## store the log model evidence
             lmes.append(copy.deepcopy(summed_lme))
 
             print(f"Iteration: {iteration + 1}, LME: {summed_lme}")
@@ -143,7 +165,7 @@ class EmpiricalBayes:
             "parameters": self.optimiser.model.parameters,
         }
 
-        # print(f"Optimisation finished in {iteration} iterations: {output}")
+        print(f"Optimisation finished in {iteration} iterations: {output}")
         return output
 
     def optimise(self):

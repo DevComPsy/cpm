bf23eb655ef6421ce0c48272f15e1c4dbba3e502
Lenard Dome <23503601+lenarddome@users.noreply.github.com>
2024-06-03 15:25:44 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ history = open("CHANGELOG.md").read().replace(".. :changelog:", "")
 
 setup(
     name="cpm",
-    version="0.15.2",
+    version="0.15.3",
     description="cpm",
     long_description=readme + "\n\n" + doclink + "\n\n" + history,
     author="Lenard Dome, Frank Hezemans, Andrew Webb, Tobias Hauser",

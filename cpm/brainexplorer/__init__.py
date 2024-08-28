29f2f36d21e9573a1e26c1065289160d8d52c24a
lenarddome <lenarddome@gmail.com>
2024-08-28 09:48:50 +0200

    start some skeletal preparations

--- cpm/brainexplorer/__init__.py
+++ cpm/brainexplorer/__init__.py
@@ -1,1 +1,4 @@
-import .spacebus
+from cpm.brainexplorer import stopsignal
+from cpm.brainexplorer import perceptual
+from cpm.brainexplorer import riskychoices
+from cpm.brainexplorer import bandits

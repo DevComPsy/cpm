bugfix: fix categorical loglikelihood bug

cpm/optimisation/minimise.py
@@ -34,20 +34,26 @@ class LogLikelihood:
         -----
 
         `predicted` and `observed` must have the same shape.
-        `observed` is a binary variable, so it can only take the values 0 or 1.
+        `observed` is a vector of integers starting from 0 (first possible response), where each integer corresponds to the observed value.
         If there are two choice options, then observed would have a shape of (n, 2) and predicted would have a shape of (n, 2).
         On each row of `observed`, the array would have a 1 in the column corresponding to the observed value and a 0 in the other column.
 
         Examples
         --------
         >>> import numpy as np
-        >>> observed = np.array([[1, 0], [0, 1], [1, 0], [0, 1]])
+        >>> observed = np.array([0, 1, 0, 1])
         >>> predicted = np.array([[0.7, 0.3], [0.3, 0.7], [0.6, 0.4], [0.4, 0.6]])
         >>> LogLikelihood.categorical(predicted, observed)
         1.7350011354094463
         """
-        values = np.array(predicted * observed).flatten()
+        observed_format = np.apply_along_axis(
+            lambda x: np.eye(observed.max() + 1)[x], 0, observed
+        )
+        observed_format = np.concatenate(observed_format, axis=0).reshape(-1, 2)
+        values = np.array(predicted * observed_format).flatten()
         values = values[values != 0]
+        values = values.sum(axis=1)
+        np.clip(values, 1e-100, 1 - 1e-100, out=values)
         # Compute the negative log likelihood
         LL = np.sum(np.log(values))
         if negative:
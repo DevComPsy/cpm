c933e9297699f076a7f465c0f530db1684a10013
lenarddome <lenarddome@gmail.com>
2025-06-27 12:45:27 +0200

    fix: update version to 0.23.5 and change Softmax/Sigmoid input shape requirements to 1D only

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ history = open("CHANGELOG.md").read().replace(".. :changelog:", "")
 
 setup(
     name="cpm",
-    version="0.23.3",
+    version="0.23.5",
     description="cpm",
     long_description=readme + "\n\n" + doclink + "\n\n" + history,
     author="Lenard Dome, Frank Hezemans, Andrew Webb, Marc Carrera, Tobias Hauser",

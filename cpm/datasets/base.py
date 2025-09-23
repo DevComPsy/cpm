52afcd5ff2fdc4303259601ddf4266efd53d0a07
FloorJBurghoorn <96433277+FloorJBurghoorn@users.noreply.github.com>
2025-09-23 16:46:08 +0200

    feat: extended documentation datasets
    Extended the documentation for the risky decision-making and metacognition datasets.

--- cpm/datasets/base.py
+++ cpm/datasets/base.py
@@ -57,7 +57,6 @@ def load_bandit_data():
     """
     return load_csv("bandit_small.csv")
 
-
 def load_risky_choices():
     """
     Load the risky choices dataset from a CSV file.
@@ -72,8 +71,20 @@ def load_risky_choices():
     >>> data = load_risky_choices()
     >>> print(data.head())
 
+
     Notes
     -----
+    
+    The data are from a risky decision-making task called "Scavenger". On each trial, participants are presented with two stranded spaceships, and are instructed to salvage one of the two ships and observe the outcome of this action. 
+    They have a radar showing them what the possible outcomes are for each spaceship, and what the probabilities are of obtaining these outcomes.
+    One of the spaceships always returns an outcome with 100% probability (i.e., the safe option). The other spaceship has a 50% probability of returning an outcome of 0, and a 50% probability of returning a different outcome (i.e., the risky option). 
+    After selecting one of the two options, participants receive feedback on the resulting outcome. 
+    In gain trials, all possible outcomes are positive (except the outcome of 0). In loss trials, all possible outcomes are negative (except the outcome of 0).  
+    In ambiguous trials, the radar provides only partial information about the probabilities of the possible outcomes of the risky choice option. In these trials, the risky choice option is shown to have at least a 25% probability of returning an outcome of 0, and at least a 25% probability of returning a different outcome. 
+    Unbeknownst to the participant, the true probability of both outcomes is always 50/50 (which is why the risky_probability values in the dataset are always 0.5).  
+    Participants play one block with 40 trials, including 10 non-ambiguous gain trials, 10 non-ambiguous loss trials, 10 ambiguous gain trials, and 10 ambiguous loss trials.  
+
+    
     The dataset will include the following columns:
 
         - `ppt`: the participant number.
@@ -89,3 +100,41 @@ def load_risky_choices():
 
     """
     return load_csv("risky_choices.csv")
+
+
+def load_metacognition_data():
+    """
+    Load the metacognition dataset from a CSV file.
+
+    Returns
+    -------
+    pd.DataFrame
+        A pandas DataFrame containing the metacognition data.
+
+    Examples
+    --------
+    >>> data = load_metacognition_data()
+    >>> print(data.head())
+
+    Notes
+    -----
+    
+    The data are from a perceptual metacognitive task called "Space Observer" (see Marzuki et al., 2025; Moses-Payne et al., 2021). The task assesses the ability to accurately reflect on and evaluate one’s own cognitive processes, known as metacognition.  
+    In each trial of the game, participants are presented with a planet in the centre of the screen. For a very brief time window (250 ms), 68 aliens appear into view, overlaying the planet. 
+    Aliens can be one of eight colours; two colours are selected at random for each trial. Aliens are identical except for their colour. 
+    After the 68 aliens disappear, participants are presented with an example of the two aliens and asked to choose which of the two had been more abundant on that trial. 
+    After each of the trials, they are asked to indicate how confident they are about their decision on a visual analogue scale ranging from “totally guessing” (0) to “totally certain” (100). 
+    A staircase procedure is used throughout the task to determine the stimulus intensity or evidence strength, defined as the difference in aliens required to keep participants’ performance at approximately 70%. 
+
+    
+    The dataset will include the following columns:
+
+        - `participant`: the participant number.
+        - `signal`: which alien is more abundant (0 = S1, 1 = S2).
+        - `stimulus_intensity`: absolute difference in number of aliens (i.e., evidence strength). 
+        - `response`: participant's choice (0 = S1, 1 = S2).
+        - `confidence`: participant's confidence rating for their response.
+        - `accuracy`: accuracy of the participant's response (0 = incorrect, 1 = 1 correct).
+
+    """
+    return load_csv("metacognition_data.csv")
